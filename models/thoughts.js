157bbda2ba5c0c85e5bf73aca70a9b8425a23424
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-04-18 11:04:24 -0500

    update for ask BCS

---
+++ models/thoughts.js
@@ -0,0 +1,3 @@
+const { Schema, Types } = require("mongoose");
+
+const 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express"); //requiring express and pointing to db route
 const db = require("./config/connection");
-const routes = require ("./routes")
+// const routes = require ("./routes")
 //returns the value of directory where we run the node process
 // const cwd = process.cwd();
 //port
@@ -11,10 +11,11 @@ const app = express();
 
   app.use(express.urlencoded({ extended: true }));// express middleware
   app.use(express.json());
-  app.use(routes)
+  // app.use(routes)
  
   db.once("open", () => {
     app.listen(PORT, () => {
-      console.log(` API listening running on port ${PORT}!`)
+      console.log(` API listening at http://localhost:${PORT}!`)
     });
   });
+
