c47b67ed3de44593f404a368967b890eb0fe6fbd
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-04-20 13:30:54 -0500

    get thought and update to user

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -18,14 +18,24 @@
         try {
         const dbThoughts = await thought.create(req.body); 
         console.log(req.body)
-        
+        const userInfo = await user.findOneAndUpdate(
+            {_id: body.userId},
+            { $push: { thoughts: _id } },
+            { new: true }
+        );
+
+        if(!userInfo) {
+            return res.status(404).json ({
+                message: "thought created but no user found with that ID",
+            })
+        }
+
         res.json(dbThoughts);
         } catch(err) {
             res.status(500).json(err);
 
         }
     },
-
 
 
 
